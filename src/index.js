68914b4312bf2b977f2265139d0fc7b1be430832
andrewc2020 <andrewc2020@gmail.com>
2020-10-04 14:35:26 +0100

    added testing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import logMessage from './js/logger'
 import './css/style.css'
 // Log message to console
-logMessage('Welcome to Expack!')
+logMessage('A very warm welcome to Expack!')
+// Needed for Hot Module Replacement
+// eslint-disable-next-line no-undef
+if(typeof(module.hot) !== 'undefined') {
+  module.hot.accept() // eslint-disable-line no-undef  
+}
