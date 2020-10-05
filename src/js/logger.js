045dc91a7670fa2e3a770a6236762cefbf8d141f
andrewc2020 <andrewc2020@gmail.com>
2020-10-05 18:14:01 +0100

    removed generated js

--- src/js/logger.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var logMessage = function (msg) { return console.log(msg); }; // eslint-disable-line no-undef
-exports.default = logMessage;
