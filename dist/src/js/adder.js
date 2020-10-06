cf26f4a180c9b7892df692788857dfff645f3a0f
andrewc2020 <andrewc2020@gmail.com>
2020-10-06 15:14:32 +0100

    clean up

--- dist/src/js/adder.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var adder = function (x, y) { x + y; };
-function default_1(x, y) {
-    return x + y;
-}
-exports.default = default_1;
